bdd139aeda72ea32cfadd758e555bf652c2aa79f
K4L1B3 <luizhlimagomes28@gmail.com>
2024-11-18 21:32:50 -0300

    first-commit

---
+++ src/main/java/com/api/stock/config/CorsConfig.java
@@ -0,0 +1,23 @@
+package com.api.stock.config; // ou com.api.stock.config, caso tenha criado o subpacote
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("http://localhost:4000"); // Endereço do seu front-end
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+}
